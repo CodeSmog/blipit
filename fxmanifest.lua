998658701874740b8aae1a6245dfbe970b233b0c
CodeSmog <103666275+CodeSmog@users.noreply.github.com>
2022-07-15 05:15:54 +0300

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,7 @@
+author "Smog"
+description "Smog - Blips"
 fx_version "cerulean"
 game "gta5"
-description "Helpot Blipit, Created by: Smog"
 
 client_scripts {"coordit.lua", "main.lua"}
 server_script "serveri.lua"
